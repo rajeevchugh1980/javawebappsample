13e929243596be1bb1a55b467082ded864530b2e
rajeevchugh1980 <65657727+rajeevchugh1980@users.noreply.github.com>
2020-06-26 22:47:22 +0530

    Update Hello.java

--- src/main/java/ms/kenchen/Calculator/Hello.java
+++ src/main/java/ms/kenchen/Calculator/Hello.java
@@ -3,7 +3,7 @@ package ms.kenchen.Calculator;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class HelloWorld {
+public class Hello {
 
   public static void main(final String[] args) {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
